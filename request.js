49361d847a53ec926b8a36b8f2604fe38ab2ce6d
Toby Koala <tobykoala@Tobys-MacBook-Pro.local>
2017-06-20 15:05:41 -0500

    working on some js for the request page

---
+++ request.js
@@ -0,0 +1,6 @@
+//This code determines the total for the user depending on what options they have chsoen on the requestPage
+
+function yourTotal() {
+  var stylePrice = document.getElementById('style').value;
+  console.log('stylePrice');
+}
